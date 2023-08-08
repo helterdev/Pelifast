4571656791b817f2589eae4b729725ea3df6b998
Helter <heltherv@gmail.com>
2023-08-07 21:25:59 -0500

    Culminado diseño de mostrar peliculas

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 const key = '435bb9c8bcd17bf79d6141d67edc22f1';
 
 /* home movies */
-const requestMovies = (page) => {
-    return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=es-MX&page=${page}`)
+const requestMovies = (page, abortSignal) => {
+    return fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=es-MX&page=${page}`,{signal: abortSignal})
 }
 
 /* export endpoints */
--- src/components/ListMovies.jsx
+++ src/components/ListMovies.jsx
@@ -5,22 +5,41 @@ const ListMovies = () => {
     const [page, setPage] = useState(1);
     const {requestMovies} = endPoints;
     useEffect( () => {
-        requestMovies(page)
-        .then(response => {
-            console.log(response);
-            if(response.ok){
-                return response.json()
-            }else{
-                throw new Error(response.status)
-            }
-        })
-        .then(data => setCurrentMovies(data.results))
-        .catch(error => console.error(error))
+        const abortController = new AbortController();
+        const getData = async () => {
+            try {
+                const response = await requestMovies(page, abortController.signal);
+                if(response.ok){
+                    const data = await response.json();
+                    setCurrentMovies(data.results);
+                }else{
+                    throw new Error(response.status)
+                }
+            } catch (error) {
+                if(!abortController.signal.aborted){
+                    console.error(error.message);
+                }
+            } 
+         }
+         getData();
+    return () => abortController.abort();
     },[page])
-    console.log(currentMovies);
     return (
-        <section>
-            <button onClick={() => {setPage(page+ 1)}}>+</button>
+        <section className="container">
+            {currentMovies.map((movie) => {
+                const {title, poster_path, id} = movie;
+                return (
+                    <a href="#" className="container__card" key={id}>
+                        <div className="container__head">
+                            <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt="" className="container__img" />
+                        </div>
+                        <h3 className="container__title">{title}</h3>
+                    </a>
+                    
+                )
+            })}
+            <button onClick={() => {setPage( page + 1)}}>siguiente</button>
+            <button onClick={() => {setPage( page - 1)}}>Anterior</button>
         </section>
     )
 }
