e2a7dd3c69558b30e2cb05e62c580411f2b121ce
Helter <heltherv@gmail.com>
2023-09-10 12:48:48 -0500

    fix: Environment Variables

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,15 @@
-const key = '435bb9c8bcd17bf79d6141d67edc22f1';
+const API_KEY = import.meta.env.VITE_API_KEY;
 /* request */
 const requestMovies = (endPoin, category, page, abortSignal) => {
-    return fetch(`https://api.themoviedb.org/3/${endPoin}/${category}?api_key=${key}&language=es-MX&page=${page}`,{signal: abortSignal})
+    return fetch(`https://api.themoviedb.org/3/${endPoin}/${category}?api_key=${API_KEY}&language=es-MX&page=${page}`,{signal: abortSignal})
 }
 
 const infoMovie = (endPoint,id, abortSignal) => {
-    return fetch(`https://api.themoviedb.org/3/${endPoint}/${id}?api_key=${key}&language=es-MX`,{signal: abortSignal})
+    return fetch(`https://api.themoviedb.org/3/${endPoint}/${id}?api_key=${API_KEY}&language=es-MX`,{signal: abortSignal})
 }
 
 /* export endpoints */
  export const endPoints = {
     requestMovies,
-    infoMovie
+    infoMovie,
 }
